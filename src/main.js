9dcfce759ac1ecca5e77c80f9992bc867a529afe
Dawid <qkari177@gmail.com>
2024-08-23 12:08:20 +0200

    js

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const counterValue = document.querySelector('.counter--js');
+const addButton = document.querySelector('.add--js');
+const subtractButton = document.querySelector('.subtract--js')
+
+let glasses = 0;
+
+const key = new Date().toISOString().slice(0,10);
+if (localStorage.getItem(key)){
+glasses = parseInt(localStorage.getItem(key));
+}
+
+const setCounterValue =  (value)=>{
+    counterValue.innerHTML=value;
+    localStorage.setItem(key, value)
+}
+
+addButton.addEventListener('click',()=>{
+    glasses = glasses + 1;
+    setCounterValue(glasses);
+})
+
+subtractButton.addEventListener('click',()=>{
+    if (glasses>= 1){
+    glasses = glasses - 1;
+    setCounterValue(glasses);
+    }
+})
